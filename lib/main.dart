ca9ca61eda6e2c9e352aaa7b5e31b815758320f2
William Bowers <22203232+wbowers2@users.noreply.github.com>
2019-03-30 02:06:23 -0700

    Add dynamic location

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -13,10 +15,35 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
+  LatLng _center = LatLng(45.521563, -122.677433);
+  
+  void _onMapCreated(GoogleMapController controller){
     _controller.complete(controller);
+    var location = new Location();
+    location.getLocation().then((LocationData currentLocation){
+      _controller.future.then((GoogleMapController controller){
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(currentLocation.latitude, currentLocation.longitude),
+              zoom: 16.0
+            ),
+          ),
+        );
+      });
+    });
+    location.onLocationChanged().listen((LocationData currentLocation) {
+      _controller.future.then((GoogleMapController controller){
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(currentLocation.latitude, currentLocation.longitude),
+              zoom: 16.0
+            ),
+          ),
+        );
+      });
+    });
   }
 
   @override
@@ -33,6 +60,7 @@ class _MyAppState extends State<MyApp> {
             target: _center,
             zoom: 11.0,
           ),
+          myLocationEnabled: true,
         ),
       ),
     );
